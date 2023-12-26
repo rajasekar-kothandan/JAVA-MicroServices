ba0c29d4d9d6b08ba63a0cdd7f9b6cdd60fb46e7
rajasekar kothandan <krajasekarcse007@gmail.com>
2023-12-26 20:44:57 +0530

    created project for Ribbon

---
+++ storeApp-Consumer-Eureka-Ribbon/src/main/java/com/microservice/storeAppConsumerEurekaRibbon/StoreAppConsumerEurekaRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.storeAppConsumerEurekaRibbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAppConsumerEurekaRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAppConsumerEurekaRibbonApplication.class, args);
+	}
+
+}
---
+++ storeApp-Consumer-Eureka-Ribbon/src/test/java/com/microservice/storeAppConsumerEurekaRibbon/StoreAppConsumerEurekaRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.storeAppConsumerEurekaRibbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAppConsumerEurekaRibbonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
