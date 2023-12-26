303b6bd508c4500c1d0434b0249e5cb236fb791c
rajasekar kothandan <krajasekarcse007@gmail.com>
2023-12-26 18:41:44 +0530

    Setting up Eureka server

---
+++ storeApp-Eureka-Server/src/test/java/com/microservice/storeAppEurekaServer/StoreAppEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.storeAppEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAppEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
