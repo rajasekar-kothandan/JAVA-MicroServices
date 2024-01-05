59f79b52d8cdac311cb9e57ed2d393095d78316c
rajasekar kothandan <krajasekarcse007@gmail.com>
2024-01-05 19:16:41 +0530

    Added FallBackMethods (or) CircuitBreaker Methods for GetMapping

--- storeApp-Consumer-Eureka-Ribbon-Hystrix/src/main/java/com/microservice/controller/ProductClientController.java
+++ storeApp-Consumer-Eureka-Ribbon-Hystrix/src/main/java/com/microservice/controller/ProductClientController.java
@@ -4,9 +4,10 @@ import com.microservice.domain.Product;
 import com.microservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Scope("request")
@@ -15,11 +16,35 @@ public class ProductClientController {
     @Autowired
     private ProductService productService;
 
-//    http://localhost:9996/get-products/1
-    @GetMapping("/get-products/{id}")
+    //    http://localhost:9995/get-products/1
+    @GetMapping("/get-product-id/{id}")
     public Product getProductById(@PathVariable("id") int id) {
 
         return productService.getProductById(id);
+    }
+
+    @GetMapping(value = "/get-product-name/{name}")
+    public List<Product> getProductByName(@PathVariable("name") String name) {
+
+        return productService.getProductByName(name);
+    }
+
+    @GetMapping(value = "/get-product-brand/{brand}")
+    public List<Product> getProductByBrand(@PathVariable("brand") String brand) {
+
+        return productService.getProductByBrand(brand);
+    }
+
+    @GetMapping(value = "/get-product-price/{price}")
+    public List<Product> getProductByPrice(@PathVariable("price") Double price) {
+
+        return productService.getProductByPrice(price);
+    }
+
+    @GetMapping(value = "/get-all-products")
+    public List<Product> getAllProductById() {
+
+        return productService.getAllProductById();
     }
 
 }
--- storeApp-Consumer-Eureka-Ribbon-Hystrix/src/main/java/com/microservice/service/ProductService.java
+++ storeApp-Consumer-Eureka-Ribbon-Hystrix/src/main/java/com/microservice/service/ProductService.java
@@ -5,14 +5,19 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class ProductService {
 
     /*
-    * Registry aware RestTemplate
-    * */
+     * Registry aware RestTemplate
+     * */
     @Autowired
     public RestTemplate restTemplate;
 
@@ -32,8 +37,62 @@ public class ProductService {
         return product;
     }
 
+    @HystrixCommand(fallbackMethod = "fallbackMethodForGetProductByName")
+    public List<Product> getProductByName(@PathVariable("name") String name) {
+
+        // To get Product details by name
+        List<Product> product = restTemplate.getForObject("http://product-service/products/name/" + name, ArrayList.class);
+        return product;
+    }
+
+    @HystrixCommand(fallbackMethod = "fallbackMethodForGetProductByBrand")
+    public List<Product> getProductByBrand(@PathVariable("brand") String brand) {
+
+        // To get Product details by brand
+        List<Product> product = restTemplate.getForObject("http://product-service/products/brand/" + brand, ArrayList.class);
+        return product;
+    }
+
+    @HystrixCommand(fallbackMethod = "fallbackMethodForGetProductByPrice")
+    public List<Product> getProductByPrice(@PathVariable("price") Double price) {
+
+        // To get Product details by price
+        List<Product> product = restTemplate.getForObject("http://product-service/products/price/" + price, ArrayList.class);
+        return product;
+    }
+
+    @HystrixCommand(fallbackMethod = "fallbackMethodForGetAllProducts")
+    public List<Product> getAllProductById() {
+
+        List<Product> product = restTemplate.getForObject("http://product-service/products", ArrayList.class);
+        return product;
+    }
+
+/***********     Fall_Back_Methods      ***************/
     public Product fallbackMethodForGetProductById(int id) {
 
         return new Product(id, "Monitor", "Jio", 34343.0);
     }
+
+    public List<Product> fallbackMethodForGetProductByName(String name) {
+
+        ArrayList<Product> products = new ArrayList<Product>();
+        products.add(new Product(1, name, "Jio", 34343.0));
+        return products;
+    }
+
+    public List<Product> fallbackMethodForGetProductByBrand(String brand) {
+
+        return Collections.singletonList(new Product(1, "Monitor", brand, 34343.0));
+    }
+
+    public List<Product> fallbackMethodForGetProductByPrice(Double price) {
+
+        return Collections.singletonList(new Product(1, "Monitor", "Jio", price));
+    }
+
+    public List<Product> fallbackMethodForGetAllProducts(){
+        return Collections.singletonList(new Product(1, "Monitor", "Jio", 50000.0));
+    }
+
 }
