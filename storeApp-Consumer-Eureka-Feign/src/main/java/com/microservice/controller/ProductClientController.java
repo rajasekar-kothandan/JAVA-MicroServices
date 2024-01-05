c3a2093c08cb44d354b753b1388d30a119931a2b
rajasekar kothandan <krajasekarcse007@gmail.com>
2024-01-05 17:08:07 +0530

    Added FeignClient CRUD Methods

--- storeApp-Consumer-Eureka-Feign/src/main/java/com/microservice/controller/ProductClientController.java
+++ storeApp-Consumer-Eureka-Feign/src/main/java/com/microservice/controller/ProductClientController.java
@@ -4,27 +4,73 @@ import com.microservice.domain.Product;
 import com.microservice.proxy.ProductServiceProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Scope("request")
 public class ProductClientController {
+
+    /*
+    * ProductClientController connects ProductServiceProxy
+    * */
+
     @Autowired
     ProductServiceProxy productServiceProxy;
 
-    @GetMapping("get-products/{id}")
+    // http://localhost:9996/get-products/1
+    @GetMapping("get-product-id/{id}")
     public Product getProductById(@PathVariable("id") int id) {
-        Product product = productServiceProxy.getProductById(id);
+
+        Product product = productServiceProxy.getProductByIds(id);
+        return product;
+    }
+
+    @GetMapping("get-product-name/{name}")
+    public List<Product> getProductByName(@PathVariable("name") String name) {
+
+        List<Product> product = productServiceProxy.getProductByNames(name);
+        return product;
+    }
+
+    @GetMapping("get-product-brand/{brand}")
+    public List<Product> getProductByBrand(@PathVariable("brand") String brand) {
+
+        List<Product> product = productServiceProxy.getProductByBrands(brand);
+        return product;
+    }
+
+    @GetMapping("get-product-price/{price}")
+    public List<Product> getProductByPrice(@PathVariable("price") String price) {
+
+        List<Product> product = productServiceProxy.getProductByPrices(price);
         return product;
     }
 
     @GetMapping("get-products")
     public List<Product> getAllProductById() {
-        List<Product> products = productServiceProxy.getAllProductById();
+
+        List<Product> products = productServiceProxy.getAllProductByIds();
         return products;
     }
+
+    @PostMapping("add-product")
+    public Product addProduct(@RequestBody Product product) {
+
+        Product products = productServiceProxy.addProducts(product);
+        return products;
+    }
+
+    @PutMapping("update-products/{id}")
+    public Product updateProduct(@PathVariable("id") Integer id, @RequestBody Product product) {
+        Product products = productServiceProxy.updateProducts(id, product);
+        return products;
+    }
+
+    @DeleteMapping(value = "delete-product-id/{id}")
+    public void deleteProductById(@PathVariable("id") Integer id) {
+        productServiceProxy.deleteProductByIds(id);
+    }
+
 }
--- storeApp-Consumer-Eureka-Feign/src/main/java/com/microservice/proxy/ProductServiceProxy.java
+++ storeApp-Consumer-Eureka-Feign/src/main/java/com/microservice/proxy/ProductServiceProxy.java
@@ -3,17 +3,41 @@ package com.microservice.proxy;
 import com.microservice.domain.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @FeignClient("product-service")
 public interface ProductServiceProxy {
 
+    /*
+     * ProductServiceProxy connects to ProductController(storeApp) through Eureka server Service_ID "product-service"
+     * */
+
     @GetMapping(value = "/products/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public Product getProductById(@PathVariable("id") Integer id);
+    public Product getProductByIds(@PathVariable("id") Integer id);
+
+    @GetMapping(value = "/products/name/{name}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Product> getProductByNames(@PathVariable("name") String name);
+
+    @GetMapping(value = "/products/brand/{brand}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Product> getProductByBrands(@PathVariable("brand") String brand);
+
+    @GetMapping(value = "/products/price/{price}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Product> getProductByPrices(@PathVariable("price") String price);
 
     @GetMapping(value = "/products", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<Product> getAllProductById();
+    public List<Product> getAllProductByIds();
+
+    @PostMapping(value = "/products", produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    Product addProducts(Product product);
+
+    @PutMapping(value = "/products/{id}", produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public Product updateProducts(@PathVariable("id") Integer id, @RequestBody Product product);
+
+    @DeleteMapping(value = "/products/{id}")
+    public void deleteProductByIds(@PathVariable("id") Integer id);
+
 }
