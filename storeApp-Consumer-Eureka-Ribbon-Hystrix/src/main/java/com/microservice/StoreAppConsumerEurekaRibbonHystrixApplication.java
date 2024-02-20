f6912e81c9885898a68474c0afa3a24ce7c2cc72
rajasekar kothandan <krajasekarcse007@gmail.com>
2024-02-20 14:38:03 +0530

    Implemented Hystrix-Dashboard for Ribbon Consumer

--- storeApp-Consumer-Eureka-Ribbon-Hystrix/src/main/java/com/microservice/StoreAppConsumerEurekaRibbonHystrixApplication.java
+++ storeApp-Consumer-Eureka-Ribbon-Hystrix/src/main/java/com/microservice/StoreAppConsumerEurekaRibbonHystrixApplication.java
@@ -3,7 +3,9 @@ package com.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 @SpringBootApplication
 public class StoreAppConsumerEurekaRibbonHystrixApplication {
