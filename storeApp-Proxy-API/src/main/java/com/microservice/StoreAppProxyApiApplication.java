1db1f8342e45d37eab1654a3fa3f9c7a0cab0cca
rajasekar kothandan <krajasekarcse007@gmail.com>
2024-02-21 09:18:53 +0530

    Implemented Zuul-Proxy

---
+++ storeApp-Proxy-API/src/main/java/com/microservice/StoreAppProxyApiApplication.java
@@ -0,0 +1,45 @@
+package com.microservice;
+
+import com.microservice.filters.ErrorFilter;
+import com.microservice.filters.PostFilter;
+import com.microservice.filters.PreFilter;
+import com.microservice.filters.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class StoreAppProxyApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StoreAppProxyApiApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter getPreFilter(){
+
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter getPostFilter(){
+
+		return new PostFilter();
+	}
+
+	@Bean
+	public RouteFilter getRouteFilter(){
+
+		return new RouteFilter();
+	}
+
+	@Bean
+	public ErrorFilter getErrorFilter(){
+
+		return new ErrorFilter();
+	}
+
+}
