9989b7d64bab89a9eec1ad3c639e572ac9a649cd
rajasekar kothandan <krajasekarcse007@gmail.com>
2024-02-24 16:06:31 +0530

    Implemented Retry module

--- storeApp-Consumer-Eureka-Feign-Resilience4j/src/main/java/com/microservice/proxy/ProductServiceProxy.java
+++ storeApp-Consumer-Eureka-Feign-Resilience4j/src/main/java/com/microservice/proxy/ProductServiceProxy.java
@@ -2,6 +2,7 @@ package com.microservice.proxy;
 
 import com.microservice.domain.Product;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,22 +15,27 @@ import java.util.List;
 @FeignClient(name="product-service")
 public interface ProductServiceProxy {
 
+    @Retry(name = "product-service")
     @CircuitBreaker(name = "product-service", fallbackMethod = "fallbackMethodGetProductById")
     @GetMapping(value = "/products/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public Product getProductById(@PathVariable("id") Integer id);
 
+    @Retry(name = "product-service")
     @CircuitBreaker(name = "product-service", fallbackMethod = "fallbackMethodGetProductByName")
     @GetMapping(value = "/products/name/{name}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<Product> getProductByName(@PathVariable("name") String name);
 
+    @Retry(name = "product-service")
     @CircuitBreaker(name = "product-service", fallbackMethod = "fallbackMethodGetProductByBrand")
     @GetMapping(value = "/products/brand/{brand}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<Product> getProductByBrand(@PathVariable("brand") String brand);
 
+    @Retry(name = "product-service")
     @CircuitBreaker(name = "product-service", fallbackMethod = "fallbackMethodGetProductByPrice")
     @GetMapping(value = "/products/price/{price}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<Product> getProductByPrice(@PathVariable("price") Double price);
 
+    @Retry(name = "product-service")
     @CircuitBreaker(name = "product-service", fallbackMethod = "fallbackMethodGetAllProductById")
     @GetMapping(value = "/products", produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<Product> getAllProductById();
@@ -63,7 +69,7 @@ public interface ProductServiceProxy {
     public default List<Product> fallbackMethodGetAllProductById(Throwable cause){
 
         System.out.println("Exception Raised with the message:=======> "+cause.getMessage());
-//        return return new ArrayList<Product>();
+//        return new ArrayList<Product>();
         return Collections.singletonList(new Product(1, "Monitor", "Jio", 50000.0));
     };
 
