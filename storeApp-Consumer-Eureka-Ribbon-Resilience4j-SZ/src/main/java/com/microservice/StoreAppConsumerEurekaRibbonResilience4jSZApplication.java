3c71f0a1da71ceb1266657a3e52daa6e7f66b745
rajasekar kothandan <krajasekarcse007@gmail.com>
2024-02-28 09:55:55 +0530

    update with bean for Sampler method

--- storeApp-Consumer-Eureka-Ribbon-Resilience4j-SZ/src/main/java/com/microservice/StoreAppConsumerEurekaRibbonResilience4jSZApplication.java
+++ storeApp-Consumer-Eureka-Ribbon-Resilience4j-SZ/src/main/java/com/microservice/StoreAppConsumerEurekaRibbonResilience4jSZApplication.java
@@ -3,14 +3,17 @@ package com.microservice;
 import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StoreAppConsumerEurekaRibbonResilience4jSZApplication {
+
 // SZ - means sleuth and Zipkin
 	public static void main(String[] args) {
 		SpringApplication.run(StoreAppConsumerEurekaRibbonResilience4jSZApplication.class, args);
 	}
 
+	@Bean
 	public Sampler getSampler(){
 
 //		return Sampler.create(0.5f);
