62c8267163302526edd890bceaf06f6c9132c520
rajasekar kothandan <krajasekarcse007@gmail.com>
2024-02-28 13:41:19 +0530

    Implemented Sleuth and Zipkin for Feign Consumer

---
+++ storeApp-Consumer-Eureka-Feign-Resilience4j-SZ/src/main/java/com/microservice/StoreAppConsumerEurekaFeignResilience4jSZApplication.java
@@ -0,0 +1,25 @@
+package com.microservice;
+
+import brave.sampler.Sampler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@EnableFeignClients
+@SpringBootApplication
+public class StoreAppConsumerEurekaFeignResilience4jSZApplication {
+
+	// SZ - means sleuth and Zipkin
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAppConsumerEurekaFeignResilience4jSZApplication.class, args);
+	}
+
+	@Bean
+	public Sampler getSampler(){
+
+//		return Sampler.create(0.5f);
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+}
---
+++ storeApp-Consumer-Eureka-Feign-Resilience4j-SZ/src/main/java/com/microservice/controller/ProductClientController.java
@@ -0,0 +1,68 @@
+package com.microservice.controller;
+
+import com.microservice.domain.Product;
+import com.microservice.proxy.ProductServiceProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Scope("request")
+public class ProductClientController {
+    @Autowired
+    ProductServiceProxy productServiceProxy;
+
+    private Logger logger = LoggerFactory.getLogger(ProductClientController.class);
+
+//    http://localhost:8998/get-product-id/1
+    @GetMapping("get-product-id/{id}")
+    public Product getProductById(@PathVariable("id") int id) {
+
+        logger.debug("In getProductById with ID: "+id);
+        Product product = productServiceProxy.getProductById(id);
+        logger.debug("In getProductById with return value Product: "+product);
+        return product;
+    }
+
+    @GetMapping("get-product-name/{name}")
+    public List<Product> getProductById(@PathVariable("name") String name) {
+
+        logger.debug("In getProductByName with ID: "+name);
+        List<Product> product = productServiceProxy.getProductByName(name);
+        logger.debug("In getProductByName with return value Product: "+product);
+        return product;
+    }
+
+    @GetMapping("get-product-brand/{brand}")
+    public List<Product> getProductByBrand(@PathVariable("brand") String brand) {
+
+        logger.debug("In getProductByBrand with ID: "+brand);
+        List<Product> product = productServiceProxy.getProductByBrand(brand);
+        logger.debug("In getProductByBrand with return value Product: "+product);
+        return product;
+    }
+
+    @GetMapping("get-product-price/{price}")
+    public List<Product> getProductByPrice(@PathVariable("price") Double price) {
+
+        logger.debug("In getProductByPrice with ID: "+price);
+        List<Product> product = productServiceProxy.getProductByPrice(price);
+        logger.debug("In getProductByPrice with return value Product: "+product);
+        return product;
+    }
+
+    @GetMapping("get-products")
+    public List<Product> getAllProductById() {
+
+        logger.debug("In getAllProductById ");
+        List<Product> products = productServiceProxy.getAllProductById();
+        logger.debug("In getAllProductById with return value Product: "+products);
+        return products;
+    }
+}
